1cce99b54394ddf35e644b84ec122876f89ffd2a
蔡悦烽 <caiyuefeng@xmiles.cn>
2023-12-19 09:53:20 +0800

    fix: 修改 vite 配置

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/ease-tools',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
